e5ee0cff9df96ec85d45f4f01449213de3ef0b99
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-06-01 10:10:54 +0200

    added courseinfo part2

---
+++ part2/courseinfo_continued/courseinfo_part2/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const App = () => {
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
+
+  const Header = (props) => {
+    return (
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+    );
+  };
+
+  const Content = (props) => {
+    return (
+      <div>
+        {props.parts.map((part, index) => (
+          <Part key={index} name={part.name} exercises={part.exercises} />
+        ))}
+      </div>
+    );
+  };
+
+  const Part = (props) => {
+    return (
+      <div>
+        <p>
+          {props.name} {props.exercises}
+        </p>
+      </div>
+    );
+  };
+
+  const Total = (props) => {
+    const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+
+    return (
+      <div>
+        <p>Number of exercises {totalExercises}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
