1a0c0794feaeaf01c23865a1e6a73a79eb00f793
chunkERR <osuchowski.jakub@gmail.com>
2023-06-04 11:54:49 +0200

    removed old courseinfo

--- part2/courseinfo_part2/src/App.js
+++ part2/courseinfo_part2/src/App.js
@@ -1,6 +1,3 @@
-import React from "react";
-import Course from "./components/Course";
-
 const App = () => {
   const courses = [
     {
@@ -47,10 +44,51 @@ const App = () => {
     },
   ];
 
+  const Header = ({ name }) => <h2>{name}</h2>;
+
+  const Content = ({ parts }) => (
+    <>
+      <Part parts={parts} />
+    </>
+  );
+
+  const Part = ({ parts }) => {
+    return parts.map((part) => (
+      <p key={part.id}>
+        {part.name} {part.exercises}
+      </p>
+    ));
+  };
+
+  const Total = ({ parts }) => {
+    const totalExercises = parts.map((part) => part.exercises);
+    const sumExercises = totalExercises.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    return (
+      <p>
+        <strong>total of {sumExercises} exercises</strong>
+      </p>
+    );
+  };
+
+  const Course = ({ courses }) => {
+    return courses.map((course) => {
+      return (
+        <div key={course.id}>
+          <Header name={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      );
+    });
+  };
+
   return (
     <div>
       <h1>Web development curriculum</h1>
-      <Course courses={courses} />
+      <Course courses={courses} key={courses.id} />
     </div>
   );
 };
