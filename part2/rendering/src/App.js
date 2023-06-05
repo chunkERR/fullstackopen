8b0c8b30d078e49b2f26560b23c55bd8db6b7657
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-06-05 15:50:47 +0200

    added as showall button

--- part2/rendering/src/App.js
+++ part2/rendering/src/App.js
@@ -32,6 +32,9 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
+      </button>
       <ul>
         {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
