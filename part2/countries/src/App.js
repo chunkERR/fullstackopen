a24804ea0477d84845aca211ced9bc0eb407e77f
chunkERR <osuchowski.jakub@gmail.com>
2023-06-19 22:06:37 +0200

    country app works with search button

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,22 +3,22 @@ import axios from "axios";
 
 const App = () => {
   const [value, setValue] = useState("");
-  const [rates, setRates] = useState({});
-  const [currency, setCurrency] = useState(null);
+  const [info, setInfo] = useState({});
+  const [country, setCountry] = useState(null);
 
   useEffect(() => {
-    console.log("effect run, currency is now", currency);
+    console.log("effect run, currency is now", country);
 
     // skip if currency is not defined
-    if (currency) {
-      console.log("fetching exchange rates...");
+    if (country) {
+      console.log("fetching country info...");
       axios
-        .get(`https://open.er-api.com/v6/latest/${currency}`)
+        .get(`https://restcountries.com/v3/name/${country}`)
         .then((response) => {
-          setRates(response.data.rates);
+          setInfo(response.data);
         });
     }
-  }, [currency]);
+  }, [country]);
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -26,16 +26,16 @@ const App = () => {
 
   const onSearch = (event) => {
     event.preventDefault();
-    setCurrency(value);
+    setCountry(value);
   };
 
   return (
     <div>
       <form onSubmit={onSearch}>
-        currency: <input value={value} onChange={handleChange} />
-        <button type="submit">exchange rate</button>
+        find countries: <input value={value} onChange={handleChange} />
+        <button type="submit">search</button>
       </form>
-      <pre>{JSON.stringify(rates, null, 2)}</pre>
+      <pre>{JSON.stringify(info, null, 2)}</pre>
     </div>
   );
 };
