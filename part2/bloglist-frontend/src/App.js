35ead9269a31b51d2e114b9c367954489878e69d
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-08-07 11:33:41 +0200

    added notification

--- part2/bloglist-frontend/src/App.js
+++ part2/bloglist-frontend/src/App.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
+
 
 
 
@@ -8,6 +11,10 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -15,14 +22,30 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {    
+  const handleLogin = async (event) => {    
     event.preventDefault()    
-    console.log('logging in with', username, password)  
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
   return (
     <div>
       <h2>blogs</h2>
+
+      
+      <Notification message={errorMessage} />
 
       <form onSubmit={handleLogin}>
         <div>
