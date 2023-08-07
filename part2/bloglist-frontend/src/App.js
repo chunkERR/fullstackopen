6e1dfe81f4c7e83c1fe9282e4dc42b6e44c58a05
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-08-07 12:16:51 +0200

    login added to blog

--- part2/bloglist-frontend/src/App.js
+++ part2/bloglist-frontend/src/App.js
@@ -6,71 +6,110 @@ import Notification from './components/Notification'
 
 
 
-
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-  const [errorMessage, setErrorMessage] = useState(null)
-
-
+  const [blogs, setBlogs] = useState([]);
+  const [newBlog, setNewBlog] = useState(''); // Add newBlog state
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then(blogs => setBlogs(blogs));
+  }, []);
 
-  const handleLogin = async (event) => {    
-    event.preventDefault()    
+  const handleLogin = async (event) => {
+    event.preventDefault();
 
     try {
       const user = await loginService.login({
-        username, password,
-      })
-      setUser(user)
-      setUsername('')
-      setPassword('')
+        username,
+        password,
+      });
+
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
+      setErrorMessage('Wrong credentials');
       setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+        setErrorMessage(null);
+      }, 5000);
     }
-  }
+  };
+
+  const handleBlogChange = (event) => {
+    setNewBlog(event.target.value); // Update newBlog state
+  };
+
+  const addBlog = (event) => {
+    event.preventDefault();
+
+    const blogObject = {
+      content: newBlog,
+    };
+
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog));
+        setNewBlog(''); // Clear the newBlog state after adding
+      })
+      .catch(error => {
+        setErrorMessage(error.response.data.error);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+    <div>
+      username
+        <input
+        type="text"
+        value={username}
+        name="Username"
+        onChange={({ target }) => setUsername(target.value)}
+      />
+    </div>
+    <div>
+      password
+        <input
+        type="password"
+        value={password}
+        name="Password"
+        onChange={({ target }) => setPassword(target.value)}
+      />
+    </div>
+    <button type="submit">login</button>
+  </form>      
+)
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <input
+        value={newBlog}
+        onChange={handleBlogChange}
+      />
+      <button type="submit">save</button>
+    </form>
+  );
 
   return (
     <div>
       <h2>blogs</h2>
-
-      
       <Notification message={errorMessage} />
 
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-          type='text'
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-          />
-          <div>
-          password
-          <input
-          type='text'
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-          />
-          </div>
-        </div>
-        <button type="submit">login</button>
-      </form>
+      {user === null ? loginForm() : blogForm()}
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
