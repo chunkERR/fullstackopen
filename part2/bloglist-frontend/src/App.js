a2646d7f68d7c0003b60c33acaef0f8fadd40a2f
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-08-07 11:27:09 +0200

    login handler added

--- part2/bloglist-frontend/src/App.js
+++ part2/bloglist-frontend/src/App.js
@@ -2,8 +2,12 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
+
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +15,34 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = (event) => {    
+    event.preventDefault()    
+    console.log('logging in with', username, password)  
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+          type='text'
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+          />
+          <div>
+          password
+          <input
+          type='text'
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          />
+          </div>
+        </div>
+        <button type="submit">login</button>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
