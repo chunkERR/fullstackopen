1a2ca67ff8e4949c06401a6dff71a2259b01256f
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-08-13 17:30:32 +0200

    added toggle and like button

--- part2/bloglist-frontend/src/components/Blog.js
+++ part2/bloglist-frontend/src/components/Blog.js
@@ -1,19 +1,29 @@
 import { useState } from "react"
 
 const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
   const [blogDetails, setBlogDetails] = useState(false);
-  const label = blogDetails ? 'view' : 'hide'
+  const label = blogDetails ? 'hide' : 'view'
 
   return (
-  <div>
-    {blog.title} {blog.author}
-    <br></br>
-    <button onClick={() => setBlogDetails(!blogDetails)}>{label}</button>
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={() => setBlogDetails(!blogDetails)}>{label}</button>
+      {blogDetails && 
+      <div>
+        {blog.url}
+        <br></br>
+        {blog.likes} <button>like</button>
+        </div>}
+    </div>
+  );
+};
 
-
-
-
-  </div>  
-  )
-}
-export default Blog
+export default Blog;
