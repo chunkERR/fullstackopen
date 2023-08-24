37cb497a9ab61093da7a4b3dd789cda04a140e9c
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-08-24 10:33:44 +0200

    lol

--- part2/rendering/src/components/NoteForm.js
+++ part2/rendering/src/components/NoteForm.js
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import Note from "./Note"
+import Note from './Note'
 
 const NoteForm = ({ createNote }) => {
-    const [newNote, setNewNote] = useState('')
+  const [newNote, setNewNote] = useState('')
 
-    const addNote = (event) => {
-        event.preventDefault()
-        createNote({
-            content: newNote,
-            important: true
-        })
+  const addNote = (event) => {
+    event.preventDefault()
+    createNote({
+      content: newNote,
+      important: true
+    })
 
 
-        setNewNote('')
-    }
-    return (
-      <div>
-        <h2>Create a new note</h2>
-  
-        <form onSubmit={addNote}>
-          <input
-            value={newNote}
-            onChange={event => setNewNote(event.target.value)}
-          />
-          <button type="submit">save</button>
-        </form>
-      </div>
-    )
+    setNewNote('')
   }
+  return (
+    <div>
+      <h2>Create a new note</h2>
+
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={event => setNewNote(event.target.value)}
+          placeholder='write note content here'
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
 
 
-  export default NoteForm
+export default NoteForm
