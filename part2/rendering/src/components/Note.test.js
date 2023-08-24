3aa835b19cec487ebe9c7b23516091c0fee2c623
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-08-24 10:24:24 +0200

    a

--- part2/rendering/src/components/Note.test.js
+++ part2/rendering/src/components/Note.test.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
-test('renders content', () => {
+test('renders content', async () => {
   const note = {
     content: 'Component testing is done with react-testing-library',
     important: true
   }
 
-  const { container } = render(<Note note={note} />)
+  const mockHandler = jest.fn()
 
-  const div = container.querySelector('.note')
-  expect(div).toHaveTextContent(
-    'Component testing is done with react-testing-library'
+  render(
+    <Note note={note}  toggleImportance={mockHandler} />
   )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
 
