f233a802cfd993d86b4249bf7d3f833a836f257f
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-08-26 21:15:17 +0200

    k

--- part2/rendering/cypress/e2e/note_app.cy.js
+++ part2/rendering/cypress/e2e/note_app.cy.js
@@ -1,7 +1,19 @@
 describe('Note app', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+  })
   it('front page can be opened', function() {
     cy.visit('http://localhost:3000')
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
   })
+
+  it('login form can be opened', function () {
+    cy.contains('log in').click()
+  })
+
+  
+
+  
 })
+
