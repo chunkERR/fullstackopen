377afb6839990e5cb623b93b6b2942ec2616fce8
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-08-28 14:22:47 +0200

    k

--- part2/rendering/cypress/e2e/note_app.cy.js
+++ part2/rendering/cypress/e2e/note_app.cy.js
@@ -11,6 +11,7 @@ describe('Note app', function() {
     cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')  
   })
+
   it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
@@ -26,8 +27,30 @@ describe('Note app', function() {
 
   })
 
+  it('login fails with wrong password', function() {
+    cy.contains('log in').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+    .should('contain', 'wrong credentials')
+    .and('have.css', 'color', 'rgb(255, 0, 0)')
+    .and('have.css', 'border-style', 'solid')
+
+    cy.contains('Matti Luukkainen logged in').should('not.exist')
+    })
+
+
+
   describe('when logged in', function () {
     beforeEach(function() {
+      cy.request('POST', 'http://localhost:3001/api/login', {
+        username: 'mluukkai', password: 'salainen'
+      }).then(response => {
+        localStorage.setItem('loggedNoteappUser', JSON.stringify(response.body))
+        cy.visit('http://localhost:3000')
+      })
       cy.contains('log in').click()
       cy.get('input:first').type('mluukkai')
       cy.get('input:last').type('salainen')
@@ -39,6 +62,25 @@ describe('Note app', function() {
       cy.get('input').type('a note created by cypress')
       cy.contains('save').click()
       cy.contains('a note created by cypress')
+
+      describe('and a note exists', function() {
+        beforeEach(function () {
+          cy.get('#note-button').click()
+          cy.get('input').type('another note cypress')
+          cy.contains('save').click()
+    
+          it('it can be made not important', function() {
+            cy.contains('another note cypress')
+            .contains('make not important')
+            .click()
+        
+            cy.contains('another note cypress')
+            .contains('make important')
+        })
+      })
+      })
     })
+
+
   })
-  })
+})
