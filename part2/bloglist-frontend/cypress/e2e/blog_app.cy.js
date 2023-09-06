599606fb3c721fe06802d5b39937016e9e491aad
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-09-06 13:46:00 +0200

    login test

--- part2/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part2/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -2,6 +2,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: "Kuba",
+      username: 'kuba',
+      password: 'Kojima'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -10,4 +16,26 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
   })
+
+  describe('Login', function() {
+    it("succeeds with correct credentials", function() {
+      cy.get('#toggle-button').click()
+      cy.get('#username').type('kuba')
+      cy.get('#password').type('Kojima')
+      cy.get('#login-submit').click()
+      cy.contains('Kuba logged in')
+    })
+
+    it('fails with the wrong credentials', function() {
+      cy.get('#toggle-button').click()
+      cy.get('#username').type('kuba')
+      cy.get('#password').type('wrong')
+      cy.get('#login-submit').click()
+      
+      cy.contains('wrong credentials')
+    })
+  })
+
 })
+
+
