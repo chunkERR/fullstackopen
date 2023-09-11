f729dfea14a9eff18988a5ca299923e3c936b83a
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2023-09-11 14:56:18 +0200

    tests not working

--- part2/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part2/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -70,31 +70,83 @@ describe('Blog app', function() {
     })
   })
 
+
   describe('When a blog has been created', function() {
     beforeEach(function() {
-      cy.login({ username: 'hellas', password: 'secret' })
+      cy.login({ username: 'kuba', password: 'Kojima' })
       cy.createBlog({
-        title: 'To Be Removed',
-        author: 'hellas',
+        title: 'You’re NOT gonna need it!',
+        author: 'Ron Jeffries',
         url: 'https://ronjeffries.com/xprog/articles/practices/pracnotneed//'
       })
     })
 
     it('it can be liked', function() {
       cy.contains('view').click()
-      cy.contains('likes: 0')
-      cy.get('#like').click({force: true})
-      cy.contains('likes: 1')
+      cy.get('#like').click()
 
+      cy.contains('likes: 1')
     })
 
+    it('the creator can delete it', function() {
+      cy.contains('view').click()
+      cy.contains('remove').click()
 
-    it('it can be removed', function() {
-      cy.contains('To Be Removed')
-      .get('#expand-button').click()
-      .get('#remove').click()
-      cy.on('window:confirm', () => true);
       cy.contains('removed')
-      cy.get('html').should('not.contain', 'To Be Removed')
+      cy.get('html').should('not.contain', 'You’re NOT gonna need it!')
+    })
+
+    it('a non creator can not delete a blog', function() {
+      cy.contains('logout').click()
+      cy.login({ username: 'hellas', password: 'secret' })
+      cy.contains('show').click()
+      cy.contains('delete').should('not.exist')
+    })
   })
-})
+
+  describe('When there exists several blogs', function() {
+    const blogs = [
+      { title: 'blog1', author: 'author1', url: 'google.com' },
+      { title: 'blog2', author: 'author2', url: 'google.com' },
+      { title: 'blog3', author: 'author3', url: 'google.com' },
+    ]
+
+    beforeEach(function() {
+      cy.login({ username: 'kuba', password: 'Kojima' })
+      cy.createBlog(blogs[0])
+      cy.createBlog(blogs[1])
+      cy.createBlog(blogs[2])
+    })
+
+    it('those are ordered by the likes', function() {
+      cy.contains(blogs[0].title).contains('show').click()
+      cy.contains(blogs[0].title).get('#like').as('like0')
+      cy.contains(blogs[1].title).contains('show').click()
+      cy.contains(blogs[1].title).contains('like').as('like1')
+      cy.contains(blogs[2].title).contains('show').click()
+      cy.contains(blogs[2].title).contains('like').as('like2')
+
+      cy.get('@like2').click()
+      cy.contains(blogs[2].title).contains('likes 1')
+      cy.get('@like2').click()
+      cy.contains(blogs[2].title).contains('likes 1')
+      cy.get('@like2').click()
+      cy.contains(blogs[2].title).contains('likes 2')
+      cy.get('@like2').click()
+      cy.contains(blogs[2].title).contains('likes 3')
+
+      cy.get('@like1').click()
+      cy.contains(blogs[1].title).contains('likes 1')
+      cy.get('@like1').click()
+      cy.contains(blogs[1].title).contains('likes 2')
+
+      cy.get('@like0').click()
+      cy.contains(blogs[0].title).contains('likes 1')
+
+      cy.get('.blog').eq(0).should('contain', blogs[2].title)
+      cy.get('.blog').eq(1).should('contain', blogs[1].title)
+      cy.get('.blog').eq(2).should('contain', blogs[0].title)
+    })
+
+  })
+
